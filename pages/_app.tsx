c0d04eb446a47a84e08077f7016eb61987fc2837
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-12-09 00:16:39 +0900

    Next.jsにAuth0サンプルを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''}
+      redirectUri={
+        process.env.NEXT_PUBLIC_REDIRECT_URI || 'http://localhost:8000'
+      }
+      audience={process.env.NEXT_PUBLIC_AUTH0_AUDIENCE || ''}
+      scope="read:current_user"
+      useRefreshTokens={true}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  )
 }
 
 export default MyApp
