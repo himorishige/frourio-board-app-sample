c6076eb5268a91c2bcdad2570e80ef269c529eae
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-12-19 15:23:38 +0900

    リポジトリ部分のリファクタリング

--- server/repository/posts.ts
+++ server/repository/posts.ts
@@ -1,44 +1,47 @@
 import { prisma } from '$/lib/prismaClient'
 import { Post, Prisma } from '.prisma/client'
 
-export const postsRepository = {
-  findUnique: async (id?: Post['id']) => {
-    return await prisma.post.findUnique({
-      where: { id },
-      include: {
-        author: true,
-        comment: {
-          include: {
-            owner: true
-          }
+export const findUnique = async (id?: Post['id']) => {
+  return await prisma.post.findUnique({
+    where: { id },
+    include: {
+      author: true,
+      comment: {
+        include: {
+          owner: true
         }
       }
-    })
-  },
-  findMany: async () => {
-    return await prisma.post.findMany({
-      include: {
-        author: true,
-        comment: true
-      },
-      orderBy: {
-        createdAt: 'desc'
-      }
-    })
-  },
-  create: async (body: Prisma.PostCreateInput) => {
-    return await prisma.post.create({
-      data: body
-    })
-  },
-  update: async (id: Post['id'], body: Prisma.PostUpdateInput) => {
-    return await prisma.post.update({
-      where: { id },
-      data: body
-    })
-  },
-  delete: async (id: Post['id']) =>
-    prisma.post.delete({
-      where: { id }
-    })
+    }
+  })
+}
+
+export const findMany = async () => {
+  return await prisma.post.findMany({
+    include: {
+      author: true,
+      comment: true
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+}
+
+export const create = async (body: Prisma.PostCreateInput) => {
+  return await prisma.post.create({
+    data: body
+  })
+}
+
+export const update = async (id: Post['id'], body: Prisma.PostUpdateInput) => {
+  return await prisma.post.update({
+    where: { id },
+    data: body
+  })
+}
+
+export const remove = async (id: Post['id']) => {
+  return prisma.post.delete({
+    where: { id }
+  })
 }
--- server/service/posts.ts
+++ server/service/posts.ts
@@ -1,34 +1,39 @@
 import { depend } from 'velona'
-import { postsRepository } from '$/repository/posts'
+import {
+  findUnique,
+  findMany,
+  create,
+  update,
+  remove
+} from '$/repository/posts'
 import { CreatePostBody, UpdatePostBody } from '$/validators'
 import { Post } from '.prisma/client'
 
 export const getPosts = depend(
-  { postsRepository },
-  async ({ postsRepository }, limit?: number) => {
-    return (await postsRepository.findMany()).slice(0, limit)
+  { findMany },
+  async ({ findMany }, limit?: number) => {
+    return (await findMany()).slice(0, limit)
   }
 )
 
 export const getPost = depend(
-  { postsRepository },
-  async ({ postsRepository }, id?: Post['id']) => postsRepository.findUnique(id)
+  { findUnique },
+  async ({ findUnique }, id?: Post['id']) => findUnique(id)
 )
 
 export const createPost = depend(
-  { postsRepository },
-  async ({ postsRepository }, body: CreatePostBody) => {
-    return await postsRepository.create(body)
+  { create },
+  async ({ create }, body: CreatePostBody) => {
+    return await create(body)
   }
 )
 
 export const updatePost = depend(
-  { postsRepository },
-  async ({ postsRepository }, id: Post['id'], body: UpdatePostBody) =>
-    postsRepository.update(id, body)
+  { update },
+  async ({ update }, id: Post['id'], body: UpdatePostBody) => update(id, body)
 )
 
 export const deletePost = depend(
-  { postsRepository },
-  async ({ postsRepository }, id: Post['id']) => postsRepository.delete(id)
+  { remove },
+  async ({ remove }, id: Post['id']) => remove(id)
 )
