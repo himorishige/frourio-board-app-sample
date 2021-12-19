e2303b18ba30d375ff3e495bee6c0fee59defe0a
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-12-19 10:11:35 +0900

    indexページリファクタリング

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,53 @@
+import { Comment, Post, User } from '.prisma/client'
+import { Avatar } from '@chakra-ui/avatar'
+import { ChatIcon } from '@chakra-ui/icons'
+import { Box, Flex, Heading } from '@chakra-ui/layout'
+import { Tag, TagLabel } from '@chakra-ui/tag'
+import React, { VFC } from 'react'
+import { echoLocalDateTime } from '../utils/dateUtil'
+import { WrappedLink } from './Link'
+
+type Props = {
+  post: Post & {
+    comment?: Comment[] | undefined
+  } & {
+    author?: User | null | undefined
+  }
+}
+
+export const PostItem: VFC<Props> = ({ post }) => {
+  return (
+    <Box
+      key={post.id}
+      overflow="hidden"
+      p={4}
+      border="1px"
+      borderRadius={8}
+      borderColor="gray.100"
+      my={4}
+    >
+      <Flex alignItems="center">
+        <Avatar
+          src={post.author?.icon ?? 'https://bit.ly/broken-link'}
+          size="sm"
+          name={post.author?.name ?? 'NO NAME'}
+          mr={2}
+        />
+        <WrappedLink href={`/posts/${post.id}`}>
+          <Heading size="md" d="inline">
+            {post.title}
+          </Heading>
+        </WrappedLink>
+      </Flex>
+      <Flex alignItems="center" mt={4}>
+        <Tag size="lg" colorScheme="gray" borderRadius="full">
+          <TagLabel ml={2}>{echoLocalDateTime(post.createdAt)}</TagLabel>
+          <TagLabel ml={2}>
+            <ChatIcon mr={2} />
+            {post.comment?.length}
+          </TagLabel>
+        </Tag>
+      </Flex>
+    </Box>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
-import LogoutButton from '~/src/components/LogoutButton'
 import { useAuth } from '~/src/hooks/useAuth'
 import useAspidaSWR from '@aspida/swr'
 import { apiClient } from '~/src/utils/apiClient'
 import { Layout } from '~/src/components/Layout'
-import { Box, Container, Flex, Heading, Text, VStack } from '@chakra-ui/layout'
-import { WrappedLink } from '~/src/components/Link'
-import { echoLocalDateTime } from '../utils/dateUtil'
-import { Avatar } from '@chakra-ui/avatar'
-import { Tag, TagLabel } from '@chakra-ui/tag'
-import { ChatIcon } from '@chakra-ui/icons'
-import { Loading } from '../components/Loading'
+import { Box, Text } from '@chakra-ui/layout'
+import { PostItem } from '~/src/components/PostItem'
 
 const Home: React.VFC = () => {
-  const { token, user } = useAuth()
+  const { token } = useAuth()
 
   const { data: posts } = useAspidaSWR(apiClient.posts, {
     headers: {
@@ -27,42 +21,7 @@ const Home: React.VFC = () => {
     <Layout>
       <Box>
         {posts ? (
-          posts?.map((post) => (
-            <Box
-              key={post.id}
-              overflow="hidden"
-              p={4}
-              border="1px"
-              borderRadius={8}
-              borderColor="gray.100"
-              my={4}
-            >
-              <Flex alignItems="center">
-                <Avatar
-                  src={post.author?.icon ?? 'https://bit.ly/broken-link'}
-                  size="sm"
-                  name={post.author?.name ?? 'NO NAME'}
-                  mr={2}
-                />
-                <WrappedLink href={`/posts/${post.id}`}>
-                  <Heading size="md" d="inline">
-                    {post.title}
-                  </Heading>
-                </WrappedLink>
-              </Flex>
-              <Flex alignItems="center" mt={4}>
-                <Tag size="lg" colorScheme="gray" borderRadius="full">
-                  <TagLabel ml={2}>
-                    {echoLocalDateTime(post.createdAt)}
-                  </TagLabel>
-                  <TagLabel ml={2}>
-                    <ChatIcon mr={2} />
-                    {post.comment?.length}
-                  </TagLabel>
-                </Tag>
-              </Flex>
-            </Box>
-          ))
+          posts?.map((post) => <PostItem key={post.id} post={post} />)
         ) : (
           <Text>loading...</Text>
         )}
