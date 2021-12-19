f2938e1ad81cae16f01e59f6b3ef4900dc9b1bb8
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-12-19 10:29:03 +0900

    AUTH0の読み込み先を変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,23 @@
-import '../styles/globals.css'
+import '~/src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '~/src/lib/theme'
 import { RecoilRoot } from 'recoil'
+import {
+  AUTH0_AUDIENCE,
+  AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN,
+  AUTH0_REDIRECT_URI
+} from '~/src/constants'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Auth0Provider
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
-      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''}
-      redirectUri={
-        process.env.NEXT_PUBLIC_REDIRECT_URI || 'http://localhost:8000'
-      }
-      audience={process.env.NEXT_PUBLIC_AUTH0_AUDIENCE || ''}
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
+      redirectUri={AUTH0_REDIRECT_URI}
+      audience={AUTH0_AUDIENCE}
       scope="read:current_user"
       useRefreshTokens={true}
     >
