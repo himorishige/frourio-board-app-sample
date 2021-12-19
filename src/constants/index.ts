87a007f1e0d2bce5042c44abc922d60616536ece
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-12-19 10:28:31 +0900

    環境変数を別ファイルに統一

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''
+export const AUTH0_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''
+export const AUTH0_REDIRECT_URI =
+  process.env.NEXT_PUBLIC_REDIRECT_URI || 'http://localhost:8000'
+export const AUTH0_AUDIENCE = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE || ''
